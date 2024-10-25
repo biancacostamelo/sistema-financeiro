6733c5d554b44f7cf8b0716be93d496d1fae9dbf
BIANCA COSTA DE MELO <bianca.cmelo2@senacsp.edu.br>
2024-10-24 22:08:53 -0300

    modificando estrutura da página

--- sistema/src/paginas/saidas.jsx
+++ sistema/src/paginas/saidas.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Pesquisa from "../components/pesquisa";
+import axios from "axios";
 
 const Saidas = () => {
+    
+    const [categoria, setCategoria] = useState('')
+    const [preco, setPreco] = useState('')
+    const [tipoPagamento, setTipopagamento] = useState('')
+    const [dataSaidas, setDatasaidas] = useState('')
+    const [descricao, setDescricao] = useState('')
+
+    const Handlesubmit = (evento) => {
+        evento.preventDefault()
+        const novaSaida = {categoria, preco, tipoPagamento, dataSaidas, descricao}
+        axios.post('http://localhost:3005/saidas', novaSaida)
+        .then(()=>{
+            alert('saída adicionada com sucesso!')
+            setCategoria('')
+            setPreco('')
+            setTipopagamento('')
+            setDatasaidas('')
+            setDescricao('')
+        })
+        .catch((erro)=>{
+            alert('erro ao registrar saída', erro)
+        })
+    }
+
     return(
         <div className="campo">
             <Pesquisa/>
-            <form>
+            <form onSubmit={Handlesubmit}>
                 <div className="div p-5">
                     <div className="mb-4">
                         <h2>Saidas</h2>
@@ -13,7 +38,7 @@ const Saidas = () => {
                     <div className="row mb-4">
                         <div className="col-sm-6 r campoLabel">
                             <label htmlFor="categoria" className="label">categoria</label>
-                            <select name="categoria" id="categoria" required>
+                            <select name="categoria" id="categoria" required value={categoria} onChange={(e) => setCategoria(e.target.value)}>
                                 <option value="">Selecione</option>
                                 <option value="alimentacao">Alimentação</option>
                                 <option value="moradia">Moradia</option>
@@ -31,13 +56,13 @@ const Saidas = () => {
                         </div>
                         <div className="col-sm-6 r campoLabel">
                             <label htmlFor="preco" className="label">preço</label>
-                            <input type="number" />
+                            <input type="number" value={preco} onChange={(e) => setPreco(e.target.value)} />
                         </div>
                     </div>
                     <div className="row mb-4">
                         <div className="col-sm-6 campoLabel">
                         <label htmlFor="tipoPagamento" className="label">tipo pagamento</label>
-                        <select name="tipoPagamento" id="categoria" required>
+                        <select name="tipoPagamento" id="categoria" required value={tipoPagamento} onChange={(e) => setTipopagamento(e.target.value)}>
                                 <option value="">Selecione</option>
                                 <option value="Pix">Pix</option>
                                 <option value="cartaocredito">Cartão de Crédito</option>
@@ -48,18 +73,18 @@ const Saidas = () => {
                         </div>
                         <div className="col-sm-6 campoLabel">
                             <label htmlFor="dataSaidas" className="label">data</label>
-                            <input type="date" className="input" />
+                            <input type="date" className="input" value={dataSaidas} onChange={(e) => setDatasaidas(e.target.value)} />
                         </div>
                     </div>
                     <div className="row mb-4" >
                         <div className="col-sm-12 campoLabel">
                             <label htmlFor="descricao" className="label" >descrição</label>
-                            <textarea id="descricao" name="descricao" style={{ width: '100%' , height: '100px'}}></textarea>
+                            <textarea id="descricao" name="descricao" style={{ width: '100%' , height: '100px'}} value={descricao} onChange={(e) => setDescricao(e.target.value)}></textarea>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-12">
-                            <button className="botao">Enviar</button>
+                            <button className="botao" type="submit">Enviar</button>
                         </div>
                     </div>
                 </div>
