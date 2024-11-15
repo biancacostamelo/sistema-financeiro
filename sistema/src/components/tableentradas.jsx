6fcbdc73dbec4078d50ec88e838b54eadabbe053
biancacostamelo <bianca.costamelo1@gmail.com>
2024-11-14 22:11:58 -0300

    melhorando responsividade

--- sistema/src/components/tableentradas.jsx
+++ sistema/src/components/tableentradas.jsx
@@ -18,7 +18,7 @@ const Tableentradas = () => {
 
     return (
         <>
-            <div className="campo">
+            <div className="campo"  style={{ overflow: 'scroll' }}>
                 <Pesquisa />
                 <div className="div3">
                     <h2>Entradas</h2>
@@ -29,27 +29,27 @@ const Tableentradas = () => {
                     <table className="table">
                         <thead>
                             <tr>
-                                <th className="pr-5">ID</th>
-                                <th className="pr-5">Categoria</th>
-                                <th className="pr-5">Preço</th>
-                                <th className="pr-5">Data</th>
-                                <th className="pr-5">Descrição</th>
-                                <th className="pr-5">Modificar</th>
-                                <th className="pr-5">Deletar</th>
+                                <th scope="col">ID</th>
+                                <th scope="col">Categoria</th>
+                                <th scope="col">Preço</th>
+                                <th scope="col">Data</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Modificar</th>
+                                <th scope="col">Deletar</th>
                             </tr>
                         </thead>
                         <tbody>
                             {entradas.map((entradas) => (
                                 <tr key={entradas.id}>
-                                    <th scope="row" className="pr-5">{entradas.id}</th>
-                                    <td className="pr-5">{entradas.categoria}</td>
-                                    <td className="pr-5">{entradas.preco}</td>
-                                    <td className="pr-5">{new Date(entradas.dataEntrada).toLocaleDateString('pt-BR', {
+                                    <th scope="row" className="v-a">{entradas.id}</th>
+                                    <td className="v-a">{entradas.categoria}</td>
+                                    <td className="v-a">{entradas.preco}</td>
+                                    <td className="v-a">{new Date(entradas.dataEntrada).toLocaleDateString('pt-BR', {
                                         day: '2-digit',
                                         month: '2-digit',
                                         year: 'numeric'
                                     })}</td>
-                                    <td className="pr-5">
+                                    <td className="v-a">
                                         <div
                                             style={{
                                                 maxWidth: "220px",
@@ -62,8 +62,8 @@ const Tableentradas = () => {
                                             {entradas.descricao}
                                         </div>
                                     </td>
-                                    <td className="pr-5"><Link to={`/updateentrada/${entradas.id}`} className="btn btn-primary mb-2 mt-2 align-middle">modificar</Link></td>
-                                    <td className="pr-5"><button onClick={e => Handledelete(entradas.id)} className="btn btn-danger mb-2 mt-2 align-middle">deletar</button></td>
+                                    <td className="v-a"><Link to={`/updateentrada/${entradas.id}`} className="btn btn-primary mb-2 mt-2 align-middle">modificar</Link></td>
+                                    <td className="v-a"><button onClick={e => Handledelete(entradas.id)} className="btn btn-danger mb-2 mt-2 align-middle">deletar</button></td>
                                 </tr>
                             ))}
 
